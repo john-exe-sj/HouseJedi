a988125307a3848c04f9b84f037b6ec6fa352f7c
john.exe.sj <john.exe.sanjose@gmail.com>
2024-11-09 14:27:45 -0800

    front and back end comms

--- src/backend/JurisdictionModel.py
+++ src/backend/JurisdictionModel.py
@@ -1,15 +1,34 @@
 # Import necessary classes from the uAgents library
-from uagents import Agent, Context
+from uagents import Agent, Context, Model
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+class Message(Model): 
+    message: str
  
 # Create an agent named alice
-agent = Agent(name="Bob", seed="backend_seed")
+agent = Agent(
+    name="Bob", 
+    seed="backend_seed", 
+    endpoint=["http://127.0.0.8001"], 
+    port=8001, 
+    mailbox=os.getenv("AGENT_KEY_BACKEND_MAILBOX")
+)
+
+FRONT_END_ADDR = "agent1q25keva9zp8hq3u6xr6jl8rkmadt0ddzvps3xttgkakjq0yt4ekpw05yan7"
  
 # Function to be called when the agent is started
 @agent.on_event("startup")
 async def introduce_agent(ctx: Context):
     # Print a greeting message with the agent's name and its address
-    print(f"Hello, I'm agent {ctx.name} and my address is {agent.address}.")
- 
+    print(f"Hello, I'm agent, my address is {ctx.agent.address}.")
+
+@agent.on_interval(period=2)
+async def send_message(ctx: Context): 
+    await ctx.send(FRONT_END_ADDR, message=Message(message="HI FROM THE BACK"))
 # Run the agent only when the script is executed directly
 if __name__ == "__main__":
     agent.run()
--- src/frontend/Chatbot.py
+++ src/frontend/Chatbot.py
@@ -1,14 +1,35 @@
 # Import necessary classes from the uAgents library
-from uagents import Agent, Context
+from uagents import Agent, Context, Model
+from langchain_ollama import OllamaLLM
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+llm = OllamaLLM(model=os.getenv('LLM_MODEL_NAME'))
+class Message(Model):
+    message: str
  
 # Create an agent named alice
-agent = Agent(name="alice", seed="frontend_seed")
- 
+agent = Agent(
+    name="alice", 
+    seed="frontend_seed", 
+    endpoint=["http://127.0.0.8000"], 
+    port=8000, 
+    mailbox=os.getenv("AGENT_KEY_FRONTEND_MAILBOX")
+)
+
+BACK_END_ADDR = "agent1q2wpcnltmzkckcg07cyyv080zge2vczecyx64df7hlj7x2uryvmcsjaagx8"
+
 # Function to be called when the agent is started
 @agent.on_event("startup")
 async def introduce_agent(ctx: Context):
     # Print a greeting message with the agent's name and its address
-    print(f"Hello, I'm agent {ctx.name} and my address is {agent.address}.")
+    print(f"Hello, I'm agent {ctx.agent.name} and my address is {agent.address}.")
+
+@agent.on_event("message")
+async def handle_message(ctx:Context, sender:str, message: Message): 
+    ctx.logger.info(f"Recieved message: {message.message}, from {sender}")
  
 # Run the agent only when the script is executed directly
 if __name__ == "__main__":
